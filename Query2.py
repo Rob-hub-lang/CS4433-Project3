7fa8eee345b007ceece2012fcaa87db6b13cad99
Rob-hub-lang <71186044+Rob-hub-lang@users.noreply.github.com>
2024-03-01 16:12:39 -0500

    Update Query2.py

--- Query2.py
+++ Query2.py
@@ -1,8 +1,8 @@
 from pyspark import SparkContext, SparkConf
 
 # Create SparkContext
-c = SparkConf().setAppName("Q2").setMaster("local[*]")
-sc = SparkContext(conf=c)
+conf = SparkConf().setAppName("Q2").setMaster("local[*]")
+sc = SparkContext(conf=conf)
 
 # Load data
 p_rdd = sc.textFile("PEOPLE-large.csv")
@@ -15,17 +15,17 @@ i_parsed_rdd = i_rdd.map(lambda line: line.split(",")).map(lambda x: (int(x[0]),
 # Broadcast infected data
 i_broadcast = sc.broadcast(i_parsed_rdd.collect())
 
-# Calculate close contacts
-c_rdd = p_parsed_rdd.flatMap(lambda x: [(x[0], y[0]) for y in i_broadcast.value if ((x[1]-y[1])**2 + (x[2]-y[2])**2)**0.5 <= 6])
-u_c_rdd = c_rdd.distinct()
+# Calculate close contacts and get distinct IDs
+distinct_close_contacts_rdd = p_parsed_rdd.flatMap(lambda x: [x[0] for y in i_broadcast.value if ((x[1]-y[1])**2 + (x[2]-y[2])**2) <= 6**2]) \
+                                        .distinct()
 
 # Collect the results
-result = u_c_rdd.collect()
+result = distinct_close_contacts_rdd.collect()
 
 # Print the results with labels
-print("People with Close Contacts to Infected Persons:")
-for id_pair in result:
-    print(f"Person {id_pair[0]} has at least one close contact with an infected person")
+print("Distinct IDs of People with Close Contacts to Infected Persons:")
+for person_id in result:
+    print(f"Person {person_id} had at least one close contact with an infected person")
 
 # Stop SparkContext
 sc.stop()
